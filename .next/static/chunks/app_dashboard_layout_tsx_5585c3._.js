84ac6944418a1cdce3b21b3d119ca0258034ace4
Naveenchandar <naveenchandece@gmail.com>
2025-05-13 07:05:24 +0530

    chapter 5

--- .next/static/chunks/app_dashboard_layout_tsx_5585c3._.js
+++ .next/static/chunks/app_dashboard_layout_tsx_5585c3._.js
@@ -5,7 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_dashboard_layout_tsx_5585c3._.js",
   "chunks": [
-    "static/chunks/_817682._.js"
+    "static/chunks/_a8a050._.js"
   ],
   "source": "dynamic"
 });
