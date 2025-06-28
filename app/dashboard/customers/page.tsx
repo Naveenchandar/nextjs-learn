8f1bee4ae3be22952cf099387b7d3db6f4ec3315
Naveenchandar <naveenchandece@gmail.com>
2025-06-28 23:21:48 +0530

    reinitialize changes

--- app/dashboard/customers/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function DashboardCustomersPage() {
-    return (
-        <p>Dashboard customer page</p>
-    )
-}
--- app/dashboard/invoices/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function DashboardInvoicePage() {
-    return (
-        <p>Dashboard invoice page</p>
-    )
-}
